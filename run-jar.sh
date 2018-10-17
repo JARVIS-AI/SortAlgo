5a7290d9c03fa013a96ebe8a47924dbf729a60c4
Jarvis Mercer <ams.h4ck3r@gmail.com>
2018-10-17 12:38:00 +0330

    add jar file and shell for run it

---
+++ run-jar.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [[ $java+/$&&$javac = true ]]; then
+	java -jar SortAlgorithmAnimation.jar
+fi
+
+echo "YOU DONT HAVE JAVA INSTALLED, PLEASE INSTALL JAVA MACHINE FIRST AND TRY AGAIN, THANKS"
+
